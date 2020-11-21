1de56564f0e72708077e27e90b9c61c567991a59
Alexander Thompson <71605358+athompson160@users.noreply.github.com>
2020-11-20 19:11:42 -0700

    commit of css

--- index.js
+++ index.js
@@ -1,71 +1,86 @@
-const breweryAPI = `https://api.openbrewerydb.org/breweries`;
+// JavaScript File
+// API Handling
+const breweryAPI = `https://api.openbrewerydb.org/breweries?by_postal=`;
 
-// Add PEXEL API to retrieve 10-20 random images of beer related pictures
-// and save them to local storage in an array with key 'images'
-// Below will retrieve a random image from local storage and append the image
-// to the 'breweryContainer' or 'breweryArticle' - whatever is consistent
-// with layout prescribed by Bulma for their box component.
+// Need to use the proxy below to add CORS header to request and Access-Control-Allow-Origin: * header to the response
+// * is a wildcard value to allow all domains to access the resources from the server.
+// Ref: https://medium.com/@dtkatz/3-ways-to-fix-the-cors-error-and-how-access-control-allow-origin-works-d97d55946d9
 
-const getBreweries = (city, state) => {
-	$("#breweries").empty();
-	const breweryURL = `${breweryAPI}?by_city=${encodeURIComponent(
-		city
-	)}&by_state=${encodeURIComponent(state)}`;
+const zipCodeAPI =
+	"https://cors-anywhere.herokuapp.com/https://www.zipcodeapi.com/rest/";
+const zipCodeKey =
+	"WwD56QP6XyYGYcJzczw7XZ3jSTQwpPclAWFh0YlGslIIlFAgotufCFQVvnS84P7j";
+
+const getZipCodes = (city, state) => {
+	const zipCodeURL = `${zipCodeAPI}${zipCodeKey}/city-zips.json/${city}/${state}`;
 
 	$.ajax({
-		url: breweryURL,
+		url: zipCodeURL,
 		method: "GET",
-	}).then(resArr => {
-		resArr.forEach(response => {
-			if (response) {
-				const {
-					name,
-					type,
-					street,
-					city,
-					state,
-					postal_code,
-					phone,
-					website_url,
-				} = response;
+	}).then(response => {
+		getBreweries(response.zip_codes);
+	});
+};
 
-				const breweryContainer = $("<div>").addClass("box");
-				const breweryArticle = $("<article>").addClass("media");
-				const brewery = $("<div>").addClass("content");
+const getBreweries = zipCodes => {
+	$("#breweries").empty();
+	zipCodes.forEach(zipCode => {
+		const breweryURL = `${breweryAPI}${zipCode}`;
+		$.ajax({
+			url: breweryURL,
+			method: "GET",
+		}).then(resArr => {
+			resArr.forEach(response => {
+				if (response) {
+					const {
+						name,
+						type,
+						street,
+						city,
+						state,
+						postal_code,
+						phone,
+						website_url,
+					} = response;
 
-				if (name) {
-					const breweryName = $("<p>").html(`<strong>${name}</strong>`);
-					brewery.append(breweryName);
-				}
-				if (type) {
-					const breweryType = $("<p>").text(`Type: ${type}`);
-					brewery.append(breweryType);
-				}
-				if (street) {
-					const breweryStreet = $("<p>").text(street);
-					brewery.append(breweryStreet);
-				}
-				if (city && state && postal_code) {
-					const breweryAddress = $("<p>").text(
-						`${city}, ${state} ${postal_code}`
-					);
-					brewery.append(breweryAddress);
-				}
-				if (phone) {
-					const breweryPhone = $("<p>").html(
-						`Phone: <a href='tel:${phone}'>${phone}</a>`
-					);
-					brewery.append(breweryPhone);
-				}
-				if (website_url) {
-					const breweryWebsite = $("<p>").text(`Website: ${website_url}`);
-					brewery.append(breweryWebsite);
-				}
+					const breweryContainer = $("<div>").addClass("box");
+					const breweryArticle = $("<article>").addClass("media");
+					const brewery = $("<div>").addClass("content");
 
-				breweryArticle.append(brewery);
-				breweryContainer.append(breweryArticle);
-				$("#breweries").append(breweryContainer);
-			}
+					if (name) {
+						const breweryName = $("<p>").html(`<strong>${name}</strong>`);
+						brewery.append(breweryName);
+					}
+					if (type) {
+						const breweryType = $("<p>").text(`Type: ${type}`);
+						brewery.append(breweryType);
+					}
+					if (street) {
+						const breweryStreet = $("<p>").text(street);
+						brewery.append(breweryStreet);
+					}
+					if (city && state && postal_code) {
+						const breweryAddress = $("<p>").text(
+							`${city}, ${state} ${postal_code}`
+						);
+						brewery.append(breweryAddress);
+					}
+					if (phone) {
+						const breweryPhone = $("<p>").html(
+							`Phone: <a href='tel:${phone}'>${phone}</a>`
+						);
+						brewery.append(breweryPhone);
+					}
+					if (website_url) {
+						const breweryWebsite = $("<p>").text(`Website: ${website_url}`);
+						brewery.append(breweryWebsite);
+					}
+
+					breweryArticle.append(brewery);
+					breweryContainer.append(breweryArticle);
+					$("#breweries").append(breweryContainer);
+				}
+			});
 		});
 	});
 };
@@ -76,5 +91,5 @@ $("#search-form").submit(e => {
 	const city = $("#search-city").val();
 	const state = $("#search-state").val();
 
-	getBreweries(city, state);
+	getZipCodes(city, state);
 });
